199bd5443defb8e6cb514ca284175967f5676f6f
zzxuxu <wangjianxu9515@hotmail.com>
2016-11-21 14:51:54 +0800

    首页视图控制器

---
+++ 百思不得姐/百思不得姐/classes(类文件)/Essence(精华)/controller/EssenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  EssenceViewController.h
+//  百思不得姐
+//
+//  Created by 王健旭 on 2016/11/21.
+//  Copyright © 2016年 王健旭. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface EssenceViewController : BaseViewController
+
+@end
---
+++ 百思不得姐/百思不得姐/classes(类文件)/Profile(我的)/controller/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineViewController.h
+//  百思不得姐
+//
+//  Created by 王健旭 on 2016/11/21.
+//  Copyright © 2016年 王健旭. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ProfileViewController : BaseViewController
+
+@end
