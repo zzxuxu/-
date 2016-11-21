6968f53a67ed6be74263adc00c3d14743dba56df
zzxuxu <wangjianxu9515@hotmail.com>
2016-11-21 14:12:14 +0800

    初始化项目

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 王健旭 on 2016/11/21.
+//  Copyright © 2016年 王健旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
